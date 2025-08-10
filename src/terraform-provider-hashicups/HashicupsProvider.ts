39d0dcac329de3f4e48a12d33ad44ca4c6999911
Jens Claes <896093+entropitor@users.noreply.github.com>
2025-08-10 16:20:36 +0200

    chore: extract out providerSchema to its own file

---
+++ src/terraform-provider-hashicups/HashicupsProvider.ts
@@ -0,0 +1,62 @@
+import { schema, tf, withDescription } from "./attributes.js";
+
+const coffeeAttributes = {
+  collection: tf.computed.string(),
+  color: tf.computed.string(),
+  description: tf.computed.string(),
+  id: tf.computed.number(),
+  image: tf.computed.string(),
+  ingredients: tf.computed.list({
+    ingredient_id: tf.computed.number(),
+  }),
+  name: tf.computed.string(),
+  origin: tf.computed.string(),
+  price: tf.computed.number(),
+  teaser: tf.computed.string(),
+};
+
+export const providerSchema = {
+  provider: schema({
+    host: tf.optional.string(),
+    username: tf.optional.string(),
+    password: tf.optional.string(),
+  }),
+  resourceSchemas: {
+    hashicups_order: schema({
+      id: tf.computed.number(),
+      last_updated: tf.computed.string(),
+      items: tf.required.list({
+        coffee: tf.required.object({
+          id: tf.required.number(),
+          collection: tf.computed.string(),
+          color: tf.computed.string(),
+          description: tf.computed.string(),
+          image: tf.computed.string(),
+          ingredients: tf.computed.list({
+            ingredient_id: tf.computed.number(),
+          }),
+          name: tf.computed.string(),
+          origin: tf.computed.string(),
+
+          price: tf.computed.number(),
+          teaser: tf.computed.string(),
+        }),
+        quantity: tf.required.number(),
+      }),
+    }),
+  },
+  dataSourceSchemas: {
+    hashicups_coffees: schema({
+      coffees: tf.computed
+        .list(coffeeAttributes)
+        .pipe(withDescription("The list of coffees")),
+    }).pipe(withDescription("All the coffees our coffee shop has")),
+    hashicups_order: schema({
+      id: tf.required.number(),
+      items: tf.computed.list({
+        coffee: tf.computed.object(coffeeAttributes),
+        quantity: tf.computed.number(),
+      }),
+    }),
+  },
+};
--- src/terraform-provider-hashicups/index.ts
+++ src/terraform-provider-hashicups/index.ts
@@ -16,13 +16,8 @@ import {
   ExtensionCodec,
 } from "@msgpack/msgpack";
 import { HashiCupsApiClient } from "./HashiCupsApiClient.js";
-import {
-  schema,
-  tf,
-  toTerraformSchema,
-  withDescription,
-  type ConfigFor,
-} from "./attributes.js";
+import { toTerraformSchema, type ConfigFor } from "./attributes.js";
+import { providerSchema } from "./HashicupsProvider.js";
 
 class Unknown {
   _unknown = "UnknownValue";
@@ -52,67 +47,6 @@ const decode = (value: unknown) =>
 const providerInstanceId = Math.floor(Math.random() * 1000);
 
 let client: HashiCupsApiClient | null = null;
-
-const coffeeAttributes = {
-  collection: tf.computed.string(),
-  color: tf.computed.string(),
-  description: tf.computed.string(),
-  id: tf.computed.number(),
-  image: tf.computed.string(),
-  ingredients: tf.computed.list({
-    ingredient_id: tf.computed.number(),
-  }),
-  name: tf.computed.string(),
-  origin: tf.computed.string(),
-  price: tf.computed.number(),
-  teaser: tf.computed.string(),
-};
-
-const providerSchema = {
-  provider: schema({
-    host: tf.optional.string(),
-    username: tf.optional.string(),
-    password: tf.optional.string(),
-  }),
-  resourceSchemas: {
-    hashicups_order: schema({
-      id: tf.computed.number(),
-      last_updated: tf.computed.string(),
-      items: tf.required.list({
-        coffee: tf.required.object({
-          id: tf.required.number(),
-          collection: tf.computed.string(),
-          color: tf.computed.string(),
-          description: tf.computed.string(),
-          image: tf.computed.string(),
-          ingredients: tf.computed.list({
-            ingredient_id: tf.computed.number(),
-          }),
-          name: tf.computed.string(),
-          origin: tf.computed.string(),
-
-          price: tf.computed.number(),
-          teaser: tf.computed.string(),
-        }),
-        quantity: tf.required.number(),
-      }),
-    }),
-  },
-  dataSourceSchemas: {
-    hashicups_coffees: schema({
-      coffees: tf.computed
-        .list(coffeeAttributes)
-        .pipe(withDescription("The list of coffees")),
-    }).pipe(withDescription("All the coffees our coffee shop has")),
-    hashicups_order: schema({
-      id: tf.required.number(),
-      items: tf.computed.list({
-        coffee: tf.computed.object(coffeeAttributes),
-        quantity: tf.computed.number(),
-      }),
-    }),
-  },
-};
 
 type ProviderConfig = ConfigFor<typeof providerSchema.provider>;
 const routes = (router: ConnectRouter) =>
