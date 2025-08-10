83466991aa774d245b6ef9191305ab64b6eb921e
Jens Claes <896093+entropitor@users.noreply.github.com>
2025-08-10 16:20:36 +0200

    feat: initial terraform provider commit

---
+++ src/terraform-provider-hashicups.ts
@@ -0,0 +1,84 @@
+import type { ConnectRouter } from "@connectrpc/connect";
+import { connectNodeAdapter } from "@connectrpc/connect-node";
+import http from "node:http2";
+import forge from "node-forge";
+
+import { Health } from "./gen/grpc/health/v1/health_connect.js";
+import { HealthCheckResponse_ServingStatus } from "./gen/grpc/health/v1/health_pb.js";
+import { GRPCStdio } from "./gen/plugin/grpc_stdio_connect.js";
+import { StdioData_Channel } from "./gen/plugin/grpc_stdio_pb.js";
+import { Provider } from "./gen/tfplugin6/tfplugin6.7_connect.js";
+import { generateIdentity } from "./certificate.js";
+
+const serverCapabilities = {
+  getProviderSchemaOptional: true,
+};
+const routes = (router: ConnectRouter) =>
+  router
+    .service(Provider, {
+      // getMetadata(_req) {
+      //   return {
+      //     serverCapabilities,
+      //     dataSources: [{ typeName: "hashicups_coffees" }],
+      //   };
+      // },
+      getProviderSchema(_req) {
+        return {
+          provider: {
+            block: {},
+          },
+          // serverCapabilities,
+        };
+      },
+    })
+    .service(Health, {
+      check(_req) {
+        return {
+          status: HealthCheckResponse_ServingStatus.SERVING,
+          stdio: HealthCheckResponse_ServingStatus.SERVING,
+        };
+      },
+    })
+    .service(GRPCStdio, {
+      async *streamStdio(_req) {},
+    });
+
+if (
+  process.env.TF_PLUGIN_MAGIC_COOKIE !==
+  "d602bf8f470bc67ca7faa0386276bbdd4330efaf76d1a219cb4d6991ca9872b2"
+) {
+  console.error(
+    "This binary is a plugin. These are not meant to be executed directly.\nPlease execute the program that consumes these plugins, which will\nload any plugins automatically",
+  );
+  process.exit(1);
+}
+
+if (process.env.PLUGIN_CLIENT_CERT == null) {
+  throw new Error("No plugin client cert provided");
+}
+
+const { cert: serverCertificate, keys } = generateIdentity();
+const cert = forge.pki.certificateToPem(serverCertificate);
+const key = forge.pki.privateKeyToPem(keys.privateKey);
+
+const serverCertificateString = forge.util
+  .encode64(
+    forge.asn1.toDer(forge.pki.certificateToAsn1(serverCertificate)).getBytes(),
+  )
+  // Remove padding
+  .replace(/=*$/, "");
+
+const PORT = 4001;
+const server = http
+  .createSecureServer({ cert, key }, connectNodeAdapter({ routes }))
+  .listen(PORT);
+server.on("session", (session) => {
+  session.socket.on("close", () => {
+    server.close();
+  });
+});
+console.log(`1|6|tcp|127.0.0.1:${PORT}|grpc|${serverCertificateString}`);
+
+process.on("SIGTERM", () => {
+  server.close();
+});
