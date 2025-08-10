3e7db05fcbebc9fcf9c7a791371aa73a6ee42220
Jens Claes <896093+entropitor@users.noreply.github.com>
2025-08-10 16:20:36 +0200

    chore: remove all type annotations

--- src/terraform-provider-hashicups/provider.ts
+++ src/terraform-provider-hashicups/provider.ts
@@ -1,22 +1,13 @@
 import type {
-  ApplyResourceChange_Request,
-  ConfigureProvider_Request,
   ConfigureProvider_Response,
-  ImportResourceState_Request,
-  PlanResourceChange_Request,
-  ReadDataSource_Request,
-  ReadResource_Request,
-  UpgradeResourceState_Request,
-  ValidateProviderConfig_Request,
   ValidateProviderConfig_Response,
-  ValidateResourceConfig_Request,
 } from "../gen/tfplugin6/tfplugin6.7_pb.js";
 import type { Provider as TerraformProvider } from "../gen/tfplugin6/tfplugin6.7_connect.js";
 import type { ConfigFor, Schema } from "./attributes.js";
 import { toTerraformSchema } from "./attributes.js";
 import { Effect } from "effect";
 import { decode } from "./codec.js";
-import type { HandlerContext, ServiceImpl } from "@connectrpc/connect";
+import type { ServiceImpl } from "@connectrpc/connect";
 import type { PartialMessage } from "@bufbuild/protobuf";
 import { datasource, type DataSource, type IDataSource } from "./datasource.js";
 import { resource, type IResource, type Resource } from "./resource.js";
@@ -133,20 +124,14 @@ class ProviderBuilder<
         };
       },
 
-      async validateProviderConfig(
-        req: ValidateProviderConfig_Request,
-        ctx: HandlerContext,
-      ) {
+      async validateProviderConfig(req, ctx) {
         console.error("[ERROR] validateProviderConfig", providerInstanceId);
         const decoded: ProviderConfig = decode(req.config!.msgpack);
         return await Effect.runPromise(provider.validate(decoded), {
           signal: ctx.signal,
         });
       },
-      async configureProvider(
-        req: ConfigureProvider_Request,
-        ctx: HandlerContext,
-      ) {
+      async configureProvider(req, ctx) {
         console.error("[ERROR] configureProvider", providerInstanceId);
         const decoded: ProviderConfig = decode(req.config!.msgpack);
 
@@ -157,51 +142,36 @@ class ProviderBuilder<
         return result;
       },
 
-      async validateDataResourceConfig(
-        req: ValidateResourceConfig_Request,
-        ctx: HandlerContext,
-      ) {
+      async validateDataResourceConfig(req, ctx) {
         const datasource = datasources[req.typeName];
         return datasource!.validateDataResourceConfig(req, ctx);
       },
-      async readDataSource(req: ReadDataSource_Request, ctx: HandlerContext) {
+      async readDataSource(req, ctx) {
         const datasource = datasources[req.typeName];
         return datasource!.readDataSource(req, ctx);
       },
 
-      async validateResourceConfig(
-        req: ValidateResourceConfig_Request,
-        ctx: HandlerContext,
-      ) {
+      async validateResourceConfig(req, ctx) {
         const resource = resources[req.typeName];
         return resource!.validateResourceConfig(req, ctx);
       },
-      async planResourceChange(
-        req: PlanResourceChange_Request,
-        ctx: HandlerContext,
-      ) {
+      async planResourceChange(req, ctx) {
         const resource = resources[req.typeName];
         return resource!.planResourceChange(req, ctx);
       },
-      async applyResourceChange(
-        req: ApplyResourceChange_Request,
-        ctx: HandlerContext,
-      ) {
+      async applyResourceChange(req, ctx) {
         const resource = resources[req.typeName];
         return resource!.applyResourceChange(req, ctx);
       },
-      async readResource(req: ReadResource_Request, ctx: HandlerContext) {
+      async readResource(req, ctx) {
         const resource = resources[req.typeName];
         return resource!.readResource(req, ctx);
       },
-      async importResourceState(
-        req: ImportResourceState_Request,
-        ctx: HandlerContext,
-      ) {
+      async importResourceState(req, ctx) {
         const resource = resources[req.typeName];
         return resource!.importResource(req, ctx);
       },
-      upgradeResourceState(req: UpgradeResourceState_Request) {
+      upgradeResourceState(req) {
         console.error("[ERROR] upgradeResourceState", providerInstanceId);
         return {
           upgradedState: { json: req.rawState!.json },
